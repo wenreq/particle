1f56ba16332c5805f71f2ae276b79f8ccbd57c48
294491328@qq.com <294491328@qq.com>
2021-07-22 10:18:47 +0800

    lizi

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div id="nav"></div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  color #2c3e50
+</style>
